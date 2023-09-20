74fd636f53c54c7fdf52dc9762b796acb6172e22
LiamBrem <liam.e.brem@gmail.com>
2023-09-20 14:30:04 -0400

    basic server

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express');
 
 const app = express();
+const port = 3000;
+
+
+//define index route
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+
+//listen to port
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
